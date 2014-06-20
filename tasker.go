c6734654ba78d16cbcefe4680d9e4bcd06ccc3a7
Goronmon <tgagnon@gmail.com>
2014-06-20 00:40:07 -0400

    Added postgres connection information with basic logic and templating

--- tasker.go
+++ tasker.go
@@ -1,15 +1,49 @@
 package main
 
 import (
-    "fmt"
+    _ "github.com/lib/pq"
     "net/http"
+    "html/template"
+    "github.com/jmoiron/sqlx"
+    "time"
+    "log"
 )
 
-func taskerHandler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "This is a task.")
+var tasks = []Task{}
+
+type Page struct {
+    Title string
+    Text string
 }
 
+type Task struct {
+    taskID int
+    taskName string
+    taskCreated time.Time
+    taskTimeSpent int64 
+
+}
+
+func taskerHandler(w http.ResponseWriter, r *http.Request) {
+    t, _ := template.ParseFiles("tasker.html")
+    t.Execute(w, tasks)
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+    http.Redirect(w, r, "/tasker", http.StatusFound)
+}
+
+
 func main() {
+    db, err := sqlx.Connect("postgres", "user=postgres password=mario64 host=localhost port=5432 dbname=tasker sslmode=disable")
+   
+    if err != nil {
+        log.Fatalln(err)
+    }
+
+    db.Select(&tasks, "SELECT * FROM task")
+
     http.HandleFunc("/tasker/", taskerHandler)
+    http.HandleFunc("/", rootHandler)
     http.ListenAndServe(":8080", nil)
 }
